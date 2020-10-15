f24f1cde215985ca536d732d5ad5091957249d2c
ogurel <65859404+ogurel@users.noreply.github.com>
2020-10-16 02:12:54 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     let html = document.querySelector(".page-cab");
 
     function size() {
-        html.style.minHeight = window.innerHeight + 'px';
+        html.style.minHeight = window.screen.availHeight + 'px';
     }
     setInterval(size, 2000)
 })
